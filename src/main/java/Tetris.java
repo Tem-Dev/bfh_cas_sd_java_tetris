ed3c252097795fa5707e2dc100f81cbdf6c9033c
Tem <tem.dvlpr@gmail.com>
2021-11-02 09:21:07 +0100

    Implement passing width & height to main with fallback

--- src/main/java/Tetris.java
+++ src/main/java/Tetris.java
@@ -2,9 +2,22 @@ import tetris.gui.GUI;
 
 public class Tetris {
 
+    private static final int DEFAULT_WIDTH = 10;
+    private static final int DEFAULT_HEIGHT = 15;
+
     public static void main(String[] args) {
-        GUI gui = new GUI(10, 15);
-        Game game = new Game(gui);
+        int width, height;
+
+        try {
+            width = Integer.parseInt(args[0]);
+            height = Integer.parseInt(args[1]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            width = DEFAULT_WIDTH;
+            height = DEFAULT_HEIGHT;
+        }
+
+        GUI gui = new GUI(width, height);
+        Game game = new Game(gui, width, height);
         game.start();
     }
 }
