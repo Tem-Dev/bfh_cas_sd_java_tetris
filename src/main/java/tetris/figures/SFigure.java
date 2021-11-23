ccbff32b76768d4365ae3bed74653139eb7bced9
Tem-Dev <tem.dvlpr@gmail.com>
2021-11-23 23:18:06 +0100

    Add SFigure

---
+++ src/main/java/tetris/figures/SFigure.java
@@ -0,0 +1,17 @@
+package tetris.figures;
+
+import tetris.gui.Block;
+
+public class SFigure extends Figure {
+
+    Block block2, block3, block4;
+
+    public SFigure() {
+        this.color = 6;
+        centerBlock = new Block(centerBlockX, centerBlockY, color);
+        block2 = new Block(centerBlockX - 1, centerBlockY, color);
+        block3 = new Block(centerBlockX, centerBlockY + 1, color);
+        block4 = new Block(centerBlockX + 1 , centerBlockY + 1, color);
+        this.blocks = new Block[] {centerBlock, block2, block3, block4};
+    }
+}
