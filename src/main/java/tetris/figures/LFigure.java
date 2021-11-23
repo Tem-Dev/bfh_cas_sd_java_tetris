25511270c5a33264689afe09cbfee83513d7b52b
Tem-Dev <tem.dvlpr@gmail.com>
2021-11-23 23:10:27 +0100

    Add LFigure

---
+++ src/main/java/tetris/figures/LFigure.java
@@ -0,0 +1,17 @@
+package tetris.figures;
+
+import tetris.gui.Block;
+
+public class LFigure extends Figure {
+
+    Block block2, block3, block4;
+
+    public LFigure() {
+        this.color = 3;
+        centerBlock = new Block(centerBlockX, centerBlockY, color);
+        block2 = new Block(centerBlockX - 1, centerBlockY, color);
+        block3 = new Block(centerBlockX + 1, centerBlockY, color);
+        block4 = new Block(centerBlockX + 1 , centerBlockY + 1, color);
+        this.blocks = new Block[] {centerBlock, block2, block3, block4};
+    }
+}
