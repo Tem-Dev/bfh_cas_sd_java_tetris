cb6eb0fe24ed1cf0f27a081398c18bcb76c72a0d
Tem-Dev <tem.dvlpr@gmail.com>
2021-11-23 23:12:57 +0100

    Add OFigure

---
+++ src/main/java/tetris/figures/OFigure.java
@@ -0,0 +1,22 @@
+package tetris.figures;
+
+import tetris.gui.Block;
+
+public class OFigure extends Figure {
+
+    Block block2, block3, block4;
+
+    public OFigure() {
+        this.color = 2;
+        centerBlock = new Block(centerBlockX, centerBlockY, color);
+        block2 = new Block(centerBlockX, centerBlockY + 1, color);
+        block3 = new Block(centerBlockX + 1, centerBlockY, color);
+        block4 = new Block(centerBlockX + 1 , centerBlockY + 1, color);
+        this.blocks = new Block[] {centerBlock, block2, block3, block4};
+    }
+
+    @Override
+    public void rotate(Direction direction) {
+        // O figure should not rotate
+    }
+}
